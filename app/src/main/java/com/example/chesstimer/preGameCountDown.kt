a6017279751faabe94a8cdcde75a0940085a0458
Harshith TM <tmharshith@gmail.com>
2026-09-06 12:59:29 +0530

    extension function for pre game start countdown

---
+++ app/src/main/java/com/example/chesstimer/preGameCountDown.kt
@@ -0,0 +1,34 @@
+package com.example.chesstimer
+
+import android.annotation.SuppressLint
+import android.os.CountDownTimer
+import android.view.View
+import androidx.core.content.ContextCompat
+
+/*Extension Function for a countdown overlay to display before game starts*/
+
+fun ActivityTimer.preGameCountDown() {
+    val activity = this
+    countdownOverlay.visibility = View.VISIBLE
+    countdownText.text = "3"
+    startupCountdownTimer?.cancel()
+    startupCountdownTimer = object : CountDownTimer(3000L, 1000L) {
+        @SuppressLint("SetTextI18n")
+        override fun onTick(millisUntilFinished: Long) {
+            val secondsRemaining = ((millisUntilFinished + 999L) / 1000L).toInt()
+            countdownText.text = secondsRemaining.toString()
+        }
+
+        override fun onFinish() {
+            countdownText.text = "1"
+            countdownOverlay.visibility = View.GONE
+            timerWhite.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    activity,
+                    R.color.light_green
+                )
+            )
+            startCurrentPlayerTimer()
+        }
+    }.start()
+}
