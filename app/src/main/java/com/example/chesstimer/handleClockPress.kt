cb6310e78c2ac49c134cab2b7cddd406d9500045
Harshith TM <tmharshith@gmail.com>
2026-09-06 15:04:57 +0530

    extension function to handle clock press

---
+++ app/src/main/java/com/example/chesstimer/handleClockPress.kt
@@ -0,0 +1,79 @@
+package com.example.chesstimer
+
+import androidx.core.content.ContextCompat
+import com.example.chesstimer.ActivityTimer.GameState
+import com.example.chesstimer.ActivityTimer.Player
+
+/*Extension Function to handle clock press and player switch*/
+
+fun ActivityTimer.handleClockPress(player:Player){
+    //val activity = this
+    if (gameState != GameState.RUNNING || player != currentPlayer) return
+
+    playerTimer?.cancel()
+    playerTimer = null
+
+    val timerMethods = TimerMethods()
+    if (selectedMethod == "FISCHER") {
+        when (currentPlayer) {
+            Player.WHITE -> {
+                whiteTimeSeconds = timerMethods.fischerMethod(whiteTimeSeconds,incrementSeconds)
+                timerTextWhite.text = formatTime(whiteTimeSeconds)
+            }
+
+            Player.BLACK -> {
+                blackTimeSeconds = timerMethods.fischerMethod(blackTimeSeconds,incrementSeconds)
+                timerTextBlack.text = formatTime(blackTimeSeconds)
+            }
+        }
+    }
+    if (selectedMethod == "BRONSTEIN") {
+        when (currentPlayer) {
+            Player.WHITE -> {
+                whiteTimeSeconds = timerMethods.bronsteinMethod(whiteTimeSeconds,turnStartSeconds,pauseTime,incrementSeconds)
+                timerTextWhite.text = formatTime(whiteTimeSeconds)
+            }
+
+            Player.BLACK -> {
+                blackTimeSeconds = timerMethods.bronsteinMethod(blackTimeSeconds,turnStartSeconds,pauseTime,incrementSeconds)
+                timerTextBlack.text = formatTime(blackTimeSeconds)
+            }
+        }
+        pauseTime = 0
+    }
+
+    when (currentPlayer) {
+        Player.WHITE -> {
+            currentPlayer = Player.BLACK
+            timerBlack.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.light_green
+                )
+            )
+            timerWhite.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.black_shade1
+                )
+            )
+        }
+
+        Player.BLACK -> {
+            currentPlayer = Player.WHITE
+            timerWhite.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.light_green
+                )
+            )
+            timerBlack.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.white_shade1
+                )
+            )
+        }
+    }
+    startCurrentPlayerTimer()
+}
